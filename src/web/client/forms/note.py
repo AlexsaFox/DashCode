bd7d2c49d6aa9c864835d17e85cbb0e6ad7374c1
Alexey Tarasov <tarasovion2004@gmail.com>
2022-03-26 18:47:17 +0300

    Real fix for urls in notes

--- src/web/client/forms/note.py
+++ src/web/client/forms/note.py
@@ -1,17 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, BooleanField, SubmitField,SelectField
+from wtforms import StringField, TextAreaField, BooleanField, SubmitField,SelectField, URLField
 from wtforms.validators import DataRequired, Length, URL
 
 class NoteCreateForm(FlaskForm):
     title= StringField('Title', validators=[DataRequired(), Length(max=65)])
     context = TextAreaField('Context', validators=[DataRequired()])
-    link = StringField('Link', validators=[URL()])
+    link = URLField('Link', validators=[])
     submit = SubmitField('Create')
 
 class NoteEditForm(FlaskForm):
     title= StringField('Title', validators=[DataRequired(), Length(max=65)])
     context = TextAreaField('Context', validators=[DataRequired()])
-    link = StringField('Link', validators=[URL()])
+    link = URLField('Link', validators=[])
     privacy = SelectField('Privacy', choices=[('private', 'Private'),
                                               ('public', 'Public')],
                                               validators=[DataRequired()])
