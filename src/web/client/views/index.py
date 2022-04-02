51796e9ca132051fc601c5e17fb90e6d7537a67b
KorobkoDmitrii_S102 <dleam@yandex.ru>
2022-04-02 23:56:29 +0300

    Notes: tags db tables, moved some functional on main page

--- src/web/client/views/index.py
+++ src/web/client/views/index.py
@@ -1,8 +1,12 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, request
+from client.forms import NoteRemoveForm
+from models import Note
 index_bp = Blueprint('index', __name__)
 
 
 @index_bp.get('/')
 def index_view():
+    user_notes = Note.query.filter_by(owner=request.environ['user'].id).all()
+    request.environ['user_notes'] = reversed(user_notes)
+    request.environ['note_remove_form'] = NoteRemoveForm()
     return render_template('index.html')
--- src/web/models/note.py
+++ src/web/models/note.py
@@ -2,21 +2,35 @@ from app import db
 from uuid import uuid4
 from .user import User
 
+def _get_note_uuid() -> str:
+        return str(uuid4())
 
+note_tag = db.Table('note_tags',
+    db.Column('note_id', db.String(36), db.ForeignKey('notes.id')),
+    db.Column('tag_id', db.String(36), db.ForeignKey('tags.id'))
+)
 class Note(db.Model):
 
     __tablename__ = 'notes'
 
-    def _get_note_uuid() -> str:
-        return str(uuid4())
-         
-    id = db.Column(db.String(36), name='id',primary_key=True,unique=True,nullable=False,
+    id = db.Column(db.String(36),primary_key=True,unique=True,nullable=False,
                    default=_get_note_uuid)
     title = db.Column(db.String(65),nullable=False)
     context = db.Column(db.Text(),nullable=False)
-    link = db.Column(db.Text())
+    link = db.Column(db.Text(),nullable=False)
+    tags_arr = db.relationship('Tag',secondary=note_tag,backref='note')
     owner = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
     is_private = db.Column(db.Boolean(),nullable=False,default=True)
+
+class Tag(db.Model):
+    
+    __tablename__ = 'tags'
+
+    id = db.Column(db.String(36),primary_key=True,unique=True,nullable=False,
+                default=_get_note_uuid)
+    title = db.Column(db.String(65),nullable=False)
+
+    
     
 
 
