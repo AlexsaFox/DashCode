07641744a879d9a3753036cca5fd5c5a87353f6e
KorobkoDmitrii_S102 <dLEAM@yandex.ru>
2022-04-03 10:10:58 +0300

    Notes: Forgot about if

--- src/web/client/views/index.py
+++ src/web/client/views/index.py
@@ -6,7 +6,8 @@ index_bp = Blueprint('index', __name__)
 
 @index_bp.get('/')
 def index_view():
-    user_notes = Note.query.filter_by(owner=request.environ['user'].id).all()
-    request.environ['user_notes'] = reversed(user_notes)
-    request.environ['note_remove_form'] = NoteRemoveForm()
+    if request.environ['user'] is not None:
+        user_notes = Note.query.filter_by(owner=request.environ['user'].id).all()
+        request.environ['user_notes'] = reversed(user_notes)
+        request.environ['note_remove_form'] = NoteRemoveForm()
     return render_template('index.html')
