6c747e68ac8ef06640569ab373cab95643f15eec
KorobkoDmitrii_S102 <dleam@yandex.ru>
2022-04-10 12:37:52 +0300

    Small fix for note remove and undo tags models

--- src/web/client/views/index.py
+++ src/web/client/views/index.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, request_tearing_down
-from client.forms import NoteRemoveForm,NoteCreateForm
+from client.forms import NoteRemoveForm,NoteCreateForm,NoteEditForm
 from models import Note
 index_bp = Blueprint('index', __name__)
 
@@ -13,4 +13,5 @@ def index_view():
         request.environ['user_notes'] = reversed(user_notes)
         request.environ['note_remove_form'] = NoteRemoveForm()
         request.environ['note_create_form'] = NoteCreateForm()
+        request.environ['note_edit_form'] = NoteEditForm()
         return render_template('home.html')
--- src/web/models/note.py
+++ src/web/models/note.py
@@ -5,10 +5,6 @@ from .user import User
 def _get_note_uuid() -> str:
         return str(uuid4())
 
-note_tag = db.Table('note_tags',
-    db.Column('note_id', db.String(36), db.ForeignKey('notes.id')),
-    db.Column('tag_id', db.String(36), db.ForeignKey('tags.id'))
-)
 class Note(db.Model):
 
     __tablename__ = 'notes'
@@ -18,17 +14,8 @@ class Note(db.Model):
     title = db.Column(db.String(65),nullable=False)
     context = db.Column(db.Text(),nullable=False)
     link = db.Column(db.Text(),nullable=False)
-    tags_arr = db.relationship('Tag',secondary=note_tag,backref='note')
     owner = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
     is_private = db.Column(db.Boolean(),nullable=False,default=True)
-
-class Tag(db.Model):
-    
-    __tablename__ = 'tags'
-
-    id = db.Column(db.String(36),primary_key=True,unique=True,nullable=False,
-                default=_get_note_uuid)
-    title = db.Column(db.String(65),nullable=False)
 
     
     
