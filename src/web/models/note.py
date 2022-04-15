e70b36675668e483e1f0538a5ac0cd7d85b5bcd6
KorobkoDmitrii_S102 <dleam@yandex.ru>
2022-04-15 13:00:09 +0300

    Notes: add creation_date to notes model

--- src/web/models/note.py
+++ src/web/models/note.py
@@ -1,9 +1,13 @@
 from app import db
 from uuid import uuid4
 from .user import User
+import datetime
 
 def _get_note_uuid() -> str:
         return str(uuid4())
+
+def _get_token_expiration_date() -> datetime.datetime:
+        return datetime.datetime.now()
 
 class Note(db.Model):
 
@@ -16,7 +20,8 @@ class Note(db.Model):
     link = db.Column(db.Text(),nullable=False)
     owner_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
     is_private = db.Column(db.Boolean(),nullable=False,default=True)
-
+    creation_date = db.Column(db.DateTime,nullable=False, 
+                   default=_get_token_expiration_date)
     
     
 
