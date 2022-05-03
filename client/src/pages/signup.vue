fb46a3d74b1cb118cc5e8f7f09f48586b3f48780
Alexey Tarasov <tarasovion2004@gmail.com>
2022-05-03 13:45:09 +0300

    Proper validation + minor changes to localization

--- client/src/pages/signup.vue
+++ client/src/pages/signup.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { email, helpers, required, sameAs } from '@vuelidate/validators'
+import { useVuelidate } from '@vuelidate/core'
 import useAuthStore from '~/store/useAuth'
 import useErrorsStore from '~/store/useErrors'
 
@@ -6,69 +8,80 @@ const { t } = useI18n()
 const auth = useAuthStore()
 const errors = useErrorsStore()
 
-const credentials = ref({
+const credentials = reactive({
   username: '',
   email: '',
   password: '',
   confirm_password: '',
 })
 
-function onSubmit() {
-  if (credentials.value.password !== credentials.value.confirm_password) {
-    errors.addError('Password and confirm password fields do not match')
-    return
-  }
+const rules = {
+  username: {
+    required: helpers.withMessage(t('sign-up.errors.username-required'), required),
+  },
+  email: {
+    required: helpers.withMessage(t('sign-up.errors.email-invalid'), required),
+    email: helpers.withMessage(t('sign-up.errors.email-invalid'), email),
+  },
+  password: {
+    required: helpers.withMessage(t('sign-up.errors.password-required'), required),
+  },
+  confirm_password: {
+    sameAs: helpers.withMessage(
+      t('sign-up.errors.passwords-do-not-match'),
+      sameAs(computed(() => credentials.password)),
+    ),
+  },
+}
 
-  auth.register(credentials.value.username, credentials.value.email, credentials.value.password)
+const vuelidate = useVuelidate(rules, credentials)
+
+function onSubmit() {
+  errors.$reset()
+  vuelidate.value.$touch()
+
+  if (vuelidate.value.$error) {
+    for (const error of vuelidate.value.$errors)
+      errors.addError(error.$message.toString())
+  }
+  else {
+    auth.register(credentials.username, credentials.email, credentials.password)
+  }
 }
 </script>
 
 <template>
-  <div class="landing-form-container">
-    <LoginSignupText />
-    <LoginSignupForm
-      :bottom-text="t('sign-up.have-account')"
-      :bottom-link-text="t('sign-up.have-account-link')"
-      :submit-button-text="t('sign-up.submit-button')"
-      bottom-link-route="/login"
-      :submit-action="onSubmit"
-    >
-      <LoginSignupFormInput
-        :label="t('sign-up.username-label')"
-        type="text"
-        @changed="(username) => credentials.username = username"
-      />
-      <LoginSignupFormInput
-        :label="t('sign-up.email-label')"
-        type="email"
-        @changed="(email) => credentials.email = email"
-      />
-      <LoginSignupFormInput
-        :label="t('sign-up.password-label')"
-        type="password"
-        @changed="(passwd) => credentials.password = passwd"
-      />
-      <LoginSignupFormInput
-        :label="t('sign-up.confirm-password-label')"
-        type="password"
-        @changed="(passwd) => credentials.confirm_password = passwd"
-      />
-    </LoginSignupForm>
-  </div>
+  <LoginSignupForm
+    :bottom-text="t('sign-up.have-account')"
+    :bottom-link-text="t('sign-up.have-account-link')"
+    :submit-button-text="t('sign-up.submit-button')"
+    bottom-link-route="/login"
+    :submit-action="onSubmit"
+  >
+    <LoginSignupFormInput
+      v-model="credentials.username"
+      :label="t('sign-up.labels.username')"
+      type="text"
+    />
+    <LoginSignupFormInput
+      v-model="credentials.email"
+      :label="t('sign-up.labels.email')"
+      type="email"
+    />
+    <LoginSignupFormInput
+      v-model="credentials.password"
+      :label="t('sign-up.labels.password')"
+      type="text"
+    />
+    <LoginSignupFormInput
+      v-model="credentials.confirm_password"
+      :label="t('sign-up.labels.confirm-password')"
+      type="text"
+    />
+  </LoginSignupForm>
 </template>
-
-<style scoped lang="scss">
-.landing-form-container {
-  height: 100%;
-  width: 100%;
-  padding: 0 300px;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-}
-</style>
 
 <route lang="yaml">
 meta:
-  layout: landing
+  layout: login-signup
 </route>
