edac7e08e9ff9be08e2289c92c1178687afdd7f7
Alexey Tarasov <tarasovion2004@gmail.com>
2022-05-07 12:13:58 +0300

    Change ValidationError type to ExpectedError

--- api/src/db/validation.py
+++ api/src/db/validation.py
@@ -1,13 +1,15 @@
 import re
 from typing import Any
 
+from src.types import ExpectedError
+
 
 EMAIL_REGEXP = re.compile(r'(?=.{1,120}$)[^@]+@[^@]+\.[^@]+')
 USERNAME_REGEXP = re.compile(r'[a-zA-Z0-9_\-\.]{1,80}')
 PASSWORD_REGEXP = re.compile(r'.{8,}')
 
 
-class ValidationError(ValueError):
+class ValidationError(ExpectedError):
     def __init__(self, field: str, value: Any):
         self.field = field
         self.value = value
