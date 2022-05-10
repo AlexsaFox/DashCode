d92b56ef6e6aa5f77973cddc29d40df79f7112ec
Alexey Tarasov <tarasovion2004@gmail.com>
2022-05-10 11:49:54 +0300

    Ensure uploads path exists before saving file

--- api/src/db/utils.py
+++ api/src/db/utils.py
@@ -15,11 +15,19 @@ def get_engine(config: DatabaseConfiguration) -> AsyncEngine:
     return create_async_engine(config.dsn)
 
 
+def ensure_path_exists(path: str):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
 async def save_file(config: FileUploadConfiguration, file: UploadFile) -> str:
     validate_file(config, file)
     _, _, extension = file.filename.rpartition('.')
     filename = urlsafe_b64encode(token_bytes(24)).decode() + '.' + extension
-    full_path = os.path.join(BASE_DIR, config.upload_path, filename)
+    folder_path = os.path.join(BASE_DIR, config.upload_path)
+    full_path = os.path.join(folder_path, filename)
+
+    ensure_path_exists(folder_path)
     async with async_open(full_path, 'wb') as af:
         await af.write(await file.read())
     return filename
