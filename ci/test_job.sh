82dd2b75f9dabaabbfee8b05d9daa9b65cf4ce77
KorobkoDmitrii_S102 <dleam@yandex.ru>
2022-05-25 17:26:23 +0300

    Missed path for report.xml

--- ci/test_job.sh
+++ ci/test_job.sh
@@ -6,4 +6,4 @@ rm -rf .pytest_cache
 source venv/bin/activate
 pip uninstall psycopg2 -y
 pip install psycopg2
-pytest -c pyproject.toml --junitxml=report.xml
+pytest -c pyproject.toml --junitxml=../report.xml
