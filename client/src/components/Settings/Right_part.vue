eaa58af49697429f85f3ccadccb387965492bd6e
Alexey Tarasov <tarasovion2004@gmail.com>
2022-05-26 21:58:00 +0300

    Add icon to 'back' button in settings menu

--- client/src/components/Settings/Right_part.vue
+++ client/src/components/Settings/Right_part.vue
@@ -10,17 +10,17 @@ const settingsPage = useSettingsPageStore()
 <template>
   <h1>{{ t("settings.button-right-part-header") }}</h1>
   <SideButton icon="" @button-click="settingsPage.changePage(Pages.MyAccountPage)">
-    <span class="i-carbon-user" /> Account
+    <span class="i-carbon-user" /> {{ t('settings.button-right-part-account') }}
   </SideButton>
   <SideButton @button-click="settingsPage.changePage(Pages.UserProfilePage)">
-    <span class="i-carbon-edit" /> Profile
+    <span class="i-carbon-edit" />  {{ t('settings.button-right-part-profile') }}
   </SideButton>
   <SideButton @button-click="settingsPage.changePage(Pages.APITokenPage)">
-    <span class="i-carbon-api" /> API token
+    <span class="i-carbon-api" />  {{ t('settings.button-right-part-token') }}
   </SideButton>
-  <button class="standard-button" @click="router.back()">
-    <i class="fi fi-rr-sign-out" />{{ t("settings.button-right-part-back") }}
-  </button>
+  <SideButton @button-click="router.back()">
+    <span class="i-carbon:arrow-left" />{{ t("settings.button-right-part-back") }}
+  </SideButton>
 </template>
 
 <style scoped lang="scss">
