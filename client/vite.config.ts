20141c8f75f75b96af12174f4472a7c9ccd337f0
Alexey Tarasov <tarasovion2004@gmail.com>
2022-05-26 23:25:53 +0300

    Add favicon & change PWA icons

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -95,25 +95,7 @@ export default defineConfig(({ mode }) => {
         manifest: {
           name: 'DashCode',
           short_name: 'DashCode',
-          theme_color: '#ffffff',
-          icons: [
-            {
-              src: '/pwa-192x192.png',
-              sizes: '192x192',
-              type: 'image/png',
-            },
-            {
-              src: '/pwa-512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-            },
-            {
-              src: '/pwa-512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-              purpose: 'any maskable',
-            },
-          ],
+          theme_color: '#617299',
         },
       }),
 
