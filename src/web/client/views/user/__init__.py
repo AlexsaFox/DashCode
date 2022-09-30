857de885ede1229a868a44c9d696f08e621ec906
Dmitriy Korobko <dLEAM@yandex.ru>
2022-09-30 08:45:27 +0300

    Deleted unused src folder

--- src/web/client/views/user/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-from .settings import settings_bp
-from .profile import profile_bp
-
-user_web_bp = Blueprint('user', __name__)
-user_web_bp.register_blueprint(settings_bp, url_prefix='/settings')
-user_web_bp.register_blueprint(profile_bp, url_prefix='/p')
